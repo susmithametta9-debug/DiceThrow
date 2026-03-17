eb517fba7fc878d5bc4d4d8783c6b563d1eee9b5
susmithametta9-debug <susmithametta9@gmail.com>
2026-03-17 11:40:25 -0400

    die roll v1

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,14 +1,27 @@
 package edu.temple.dicethrow
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            supportFragmentManager
+                .findFragmentById(R.id.fragmentContainerView)?.run(
+                    (this as DieFragment).
+                )
+                }
+
+        }
+    )
     }
 }
